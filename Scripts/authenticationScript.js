b273ede18e087b784f2425a887f75731176c3bb5
Nicholas Yu <nyu@shaw.ca>
2019-12-05 13:34:55 -0800

    try to fix firebase host

---
+++ Scripts/authenticationScript.js
@@ -0,0 +1,34 @@
+/**
+ * checks the login satus then sets
+ * the text in the nav bar to the correct text
+ */
+// check for login
+function authentication(){
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log("Logged in as" + user);
+            document.getElementById('notifylogin').innerHTML = "you are LOGGED IN"; 
+            document.getElementById('login').innerHTML ="";
+            document.getElementById('logOut').innerHTML =  "<a class='nav-link logout' href='javascript:void(0)'>Logout</a>";
+        } else {
+            console.log("not logged in");
+            document.getElementById('notifylogin').innerHTML = "LOGIN to book appoitments";
+            document.getElementById('login').innerHTML="<a class='nav-link' href='./login.html' >Login</a>";
+            document.getElementById('logOut').innerHTML = ""
+                
+      
+      }
+    });
+
+    $(".logout").click(function (event) {
+        event.preventDefault();
+        firebase.auth().signOut().then(function () {
+          console.log('logged out');
+        }).catch(function (error) {
+            console.log('error');
+                // An error happened.
+        });
+    });
+}
+
+//deploy test
--- Scripts/pageScripts.js
+++ Scripts/pageScripts.js
@@ -1,34 +1,36 @@
-/**
- * checks the login satus then sets
- * the text in the nav bar to the correct text
- */
-// check for login
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        console.log("Logged in as" + user);
-        document.getElementById('notifylogin').innerHTML = "you are LOGGED IN"; 
-        document.getElementById('login').innerHTML ="";
-        document.getElementById('logOut').innerHTML =  "<a class='nav-link logout' href='javascript:void(0)'>Logout</a>";
-    } else {
-        console.log("not logged in");
-        document.getElementById('notifylogin').innerHTML = "LOGIN to book appoitments";
-        document.getElementById('login').innerHTML="<a class='nav-link' href='./login.html' >Login</a>";
-        document.getElementById('logOut').innerHTML = ""
+// /**
+//  * checks the login satus then sets
+//  * the text in the nav bar to the correct text
+//  */
+// // check for login
+// function authentication(){
+// firebase.auth().onAuthStateChanged(function (user) {
+//     if (user) {
+//         console.log("Logged in as" + user);
+//         document.getElementById('notifylogin').innerHTML = "you are LOGGED IN"; 
+//         document.getElementById('login').innerHTML ="";
+//         document.getElementById('logOut').innerHTML =  "<a class='nav-link logout' href='javascript:void(0)'>Logout</a>";
+//     } else {
+//         console.log("not logged in");
+//         document.getElementById('notifylogin').innerHTML = "LOGIN to book appoitments";
+//         document.getElementById('login').innerHTML="<a class='nav-link' href='./login.html' >Login</a>";
+//         document.getElementById('logOut').innerHTML = ""
             
   
-  }
-});
+//   }
+// });
   
-$("#logout").click(function (event) {
-    event.preventDefault();
-    firebase.auth().signOut().then(function () {
-      console.log('logged out');
-    }).catch(function (error) {
-        console.log('error');
-            // An error happened.
-    });
-});
+// $("#logout").click(function (event) {
+//     event.preventDefault();
+//     firebase.auth().signOut().then(function () {
+//       console.log('logged out');
+//     }).catch(function (error) {
+//         console.log('error');
+//             // An error happened.
+//     });
+// });
 
+// }
 
 /**
  * Gets a list of tutors from the databse and displays the top three.
