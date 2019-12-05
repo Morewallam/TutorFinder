64cacee2f63b292bae081a57e5921eb8f82a5c7c
Sean Wallace <swallace987@gmail.com>
2019-12-05 10:43:14 -0800

    made the nav bar smoother

--- Scripts/pageScripts.js
+++ Scripts/pageScripts.js
@@ -7,13 +7,13 @@ firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log("Logged in as" + user);
         document.getElementById('notifylogin').innerHTML = "LOGGED IN"; 
-        document.getElementById('login').innerHTML =" <a class='nav-link' href='./login.html' ></a>";
+        document.getElementById('login').innerHTML ="";
         document.getElementById('logOut').innerHTML =  "<a class='nav-link logout' href='javascript:void(0)'>Logout</a>";
     } else {
         console.log("not logged in");
         document.getElementById('notifylogin').innerHTML = "LOGIN to book appoitments";
         document.getElementById('login').innerHTML="<a class='nav-link' href='./login.html' >Login</a>";
-        document.getElementById('logOut').innerHTML = "<a class='nav-link logout' href='javascript:void(0)'></a>"
+        document.getElementById('logOut').innerHTML = ""
             
   
   }
